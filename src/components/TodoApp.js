e21bdf1a9b2e6f6ab53a4ee2c68930c4344dd724
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-12-10 22:16:53 +0100

    feat: add TodoApp

---
+++ src/components/TodoApp.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import Footer from './Footer';
+import { saveTodo } from '../lib/service';
+
+export default class TodoApp extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentTodo: '',
+            todos: [],
+            error: false,
+        };
+        this.handleNewTodoChange = this.handleNewTodoChange.bind(this);
+        this.handleTodoSubmit = this.handleTodoSubmit.bind(this);
+    }
+
+    handleNewTodoChange(event) {
+        this.setState({ currentTodo: event.target.value });
+    }
+
+    handleTodoSubmit(event) {
+        event.preventDefault();
+        const newTodo = { name: this.state.currentTodo, isComplete: false };
+        saveTodo(newTodo)
+            .then(({ data }) =>
+                this.setState({
+                    todos: this.state.todos.concat(data),
+                    currentTodo: '',
+                })
+            )
+            .catch(() => this.setState({ error: true }));
+    }
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <header className='header'>
+                        <h1>todos</h1>
+                        {this.state.error ? (
+                            <span className='error'>Error message</span>
+                        ) : null}
+                        <TodoForm
+                            currentTodo={this.state.currentTodo}
+                            handleNewTodoChange={this.handleNewTodoChange}
+                            handleTodoSubmit={this.handleTodoSubmit}
+                        />
+                    </header>
+                    <section className='main'>
+                        <TodoList todos={this.state.todos} />
+                    </section>
+                    <Footer />
+                </div>
+            </Router>
+        );
+    }
+}
