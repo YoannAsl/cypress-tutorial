5ca8742ff05b3f63f0c21b75bb6b07fed82d7a05
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-12-13 20:30:43 +0100

    test: add item deletion test

--- cypress/integration/list-items.spec.js
+++ cypress/integration/list-items.spec.js
@@ -15,4 +15,19 @@ describe('List items', () => {
     it('Shows remaining todos in the footer', () => {
         cy.get('.todo-count').should('contain', 3);
     });
+
+    it.only('Removes a todo', () => {
+        cy.route({
+            url: '/api/todos/1',
+            method: 'DELETE',
+            status: 200,
+            response: {},
+        });
+
+        cy.get('.todo-list li').as('list');
+
+        cy.get('@list').first().find('.destroy').invoke('show').click();
+
+        cy.get('@list').should('have.length', 3).and('not.contain', 'Milk');
+    });
 });
